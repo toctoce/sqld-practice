91bf0fa2b42dff5922e8d4b5a21ff74f7cc1d51d
toctoce <toctoce@naver.com>
2026-01-22 10:10:23 +0900

    itit: 프로젝트 생성

---
+++ src/test/java/toctoce/sqldpractice/SqldPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package toctoce.sqldpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqldPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
