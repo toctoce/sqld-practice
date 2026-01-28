47d240e45dfacd8fc2e4649de28711ddf864d57f
toctoce <toctoce@naver.com>
2026-01-28 10:52:08 +0900

    test(UserRepository): 유저 저장, 삭제 테스트케이스 추가 #1

--- src/test/java/toctoce/sqldpractice/domain/user/UserRepositoryTest.java
+++ src/test/java/toctoce/sqldpractice/domain/user/UserRepositoryTest.java
@@ -2,6 +2,7 @@ package toctoce.sqldpractice.domain.user;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,18 +16,35 @@ class UserRepositoryTest {
     private UserRepository userRepository;
 
     @Test
-    void findByEmail() {
-        User user = User.builder()
-                .email("test@gmail.com")
-                .nickname("testuser")
-                .role(UserRole.USER)
-                .build();
+    void 유저_삭제_테스트() {
+        User user = createTestUser();
+        userRepository.save(user);
+
+        userRepository.delete(user);
+        assertThat(userRepository.count()).isEqualTo(0);
+    }
+
+    @Test
+    void 이메일로_유저_조회가_가능하다() {
+        User user = createTestUser();
 
         userRepository.save(user);
 
-        User foundUser = userRepository.findByEmail("test@gmail.com").get();
+        // When
+        Optional<User> foundUser = userRepository.findByEmail("test@gmail.com");
 
-        assertThat(foundUser.getEmail()).isEqualTo("test@gmail.com");
-        assertThat(foundUser.getNickname()).isEqualTo("testuser");
+        // Then
+        assertThat(foundUser).isPresent(); // Optional 검증
+        assertThat(foundUser.get().getEmail()).isEqualTo("test@gmail.com");
+        assertThat(foundUser.get().getNickname()).isEqualTo("test-user");
+    }
+
+    private static User createTestUser() {
+        User user = User.builder()
+                .email("test@gmail.com")
+                .nickname("test-user")
+                .role(UserRole.USER)
+                .build();
+        return user;
     }
 }
