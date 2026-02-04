937968c89604ee1b0dd56fe46aa8aea03943575e
toctoce <toctoce@naver.com>
2026-02-05 00:23:30 +0900

    fix(SecurityConfig): 경로 추가 및 시그니처에 에러 추가 #4

--- src/main/java/toctoce/sqldpractice/global/config/SecurityConfig.java
+++ src/main/java/toctoce/sqldpractice/global/config/SecurityConfig.java
@@ -21,10 +21,11 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/", "/error", "/login", "/signup").permitAll() // 누구나 접근 가능
+                        .requestMatchers("/", "/error", "/login", "/signup", "/css/**", "/js/**")
+                        .permitAll() // 누구나 접근 가능
                         .anyRequest().authenticated() // 그 외 로그인 필수
                 )
                 .oauth2Login(oauth2 -> oauth2
