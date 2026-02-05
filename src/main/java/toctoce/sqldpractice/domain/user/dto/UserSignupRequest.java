758827d5f98e872be3b69cd138d50bf6ede84391
toctoce <toctoce@naver.com>
2026-02-05 10:30:51 +0900

    fix(UserSignupRequest): 생성자 제거 #4

--- src/main/java/toctoce/sqldpractice/domain/user/dto/UserSignupRequest.java
+++ src/main/java/toctoce/sqldpractice/domain/user/dto/UserSignupRequest.java
@@ -1,10 +1,4 @@
 package toctoce.sqldpractice.domain.user.dto;
 
 public record UserSignupRequest(String email, String password, String nickname) {
-
-    public UserSignupRequest(String email, String password, String nickname) {
-        this.email = email;
-        this.password = password;
-        this.nickname = nickname;
-    }
 }
